163d117c9b11a883350302fa79b524eace8357cd
LAPTOP-JE9T69BG\shimak <komaki289@gmail.com>
2016-10-06 00:39:14 +0900

    Unityで作成中のアプリをGitでソース管理

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy :Token {
+
+	// Use this for initialization
+	void Start () {
+        float size = Random.Range(1, 5);
+        SetSize(size, size);
+        float pos = Random.Range(0, 1);
+        SetPosition(SpriteWidth * pos, SpriteHeight);
+
+        float direction = Random.Range(0, 359);
+        float speed = 2;
+        SetVelocity(direction, speed);
+	}
+
+    /// 更新
+    void Update()
+    {
+        // カメラの左下座標を取得
+        Vector2 min = GetWorldMin();
+        // カメラの右上座標を取得する
+        Vector2 max = GetWorldMax();
+
+        if (X < min.x || max.x < X)
+        {
+            // 画面外に出たので、X移動量を反転する
+            VX *= -1;
+            // 画面内に移動する
+            //ClampScreen();
+        }
+    }
+    void OnBecameInvisible()
+    {
+        DestroyObj();
+    }
+}
---
+++ Assets/Scripts/GenerateEnemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateEnemy : MonoBehaviour {
+
+    public GameObject spawnObject;
+    public float interval = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Spawn");
+	}
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            Instantiate(spawnObject, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
